83cd794046d38e26714696cc1dc6d1e032255ca1
Cat4848 <catalinrol@gmail.com>
2024-08-30 19:49:34 +0100

    changed: InputComponent

--- app/lib/components/common/Input/Input.tsx
+++ app/lib/components/common/Input/Input.tsx
@@ -3,22 +3,23 @@ import { useState } from "react";
 interface InputComponentProps {
   label: string;
   placeholder?: string;
-  initialValue: string;
+  value: string;
+  onChange: (input: string) => void;
 }
 export default function InputComponent({
   label,
   placeholder,
-  initialValue
+  value,
+  onChange
 }: InputComponentProps) {
-  const [inputValue, setInputValue] = useState(initialValue);
   return (
     <div>
       <label htmlFor="input-element">{label}</label>
       <input
         id="input-element"
         placeholder={placeholder}
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
       />
     </div>
   );
--- app/lib/components/common/Input/__tests__/Input.test.tsx
+++ app/lib/components/common/Input/__tests__/Input.test.tsx
@@ -5,20 +5,25 @@ import userEvent from "@testing-library/user-event";
 test("if it handles user input correctly", async () => {
   const label = "Name";
   const placeholder = "Jane Smith";
-  const initialValue = "No text";
+  const value = "";
+  const handleChange = jest.fn();
+
   render(
     <InputComponent
       label={label}
       placeholder={placeholder}
-      initialValue={initialValue}
+      value={value}
+      onChange={handleChange}
     />
   );
 
   const inputElement = screen.getByLabelText(label) as HTMLInputElement;
   const user = userEvent.setup();
   const typedValue = "John March";
+  screen.debug();
 
   await user.clear(inputElement);
   await user.type(inputElement, typedValue);
-  expect(inputElement).toHaveValue(typedValue);
+  console.log("calls", handleChange.mock.calls);
+  expect(handleChange.mock.calls[0][0]).toBe(typedValue[0]);
 });
