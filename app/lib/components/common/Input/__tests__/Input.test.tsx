94ba601cfcd23a3133d83c928ba49dd08f6852a6
Cat4848 <catalinrol@gmail.com>
2024-08-30 19:51:14 +0100

    refactor: removed debugging code

--- app/lib/components/common/Input/__tests__/Input.test.tsx
+++ app/lib/components/common/Input/__tests__/Input.test.tsx
@@ -20,10 +20,8 @@ test("if it handles user input correctly", async () => {
   const inputElement = screen.getByLabelText(label) as HTMLInputElement;
   const user = userEvent.setup();
   const typedValue = "John March";
-  screen.debug();
 
   await user.clear(inputElement);
   await user.type(inputElement, typedValue);
-  console.log("calls", handleChange.mock.calls);
   expect(handleChange.mock.calls[0][0]).toBe(typedValue[0]);
 });
