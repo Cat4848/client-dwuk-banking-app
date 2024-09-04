7be8d7c763bcb966ae6d19fe82533a6eb1ce843f
Cat4848 <catalinrol@gmail.com>
2024-09-04 13:03:33 +0100

    feat: UpdateBalanceForm component init

---
+++ app/accounts/components/UpdateBalance/UpdateBalanceForm/UpdateBalanceForm.tsx
@@ -0,0 +1,10 @@
+import { FieldValues } from "react-hook-form";
+
+interface UpdateBalanceForm {
+  handleSubmit: () => void;
+  fieldValues: FieldValues;
+}
+
+export default function UpdateBalanceForm() {
+  return <form></form>;
+}
