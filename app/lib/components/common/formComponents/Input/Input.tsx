e8ad83e115f8cee4815446dea956955f4ba371c9
Cat4848 <catalinrol@gmail.com>
2024-09-05 15:54:06 +0100

    feat: removed styles sheet dependency

--- app/lib/components/common/formComponents/Input/Input.tsx
+++ app/lib/components/common/formComponents/Input/Input.tsx
@@ -1,24 +1,25 @@
 import { FieldValues, UseFormRegister } from "react-hook-form";
 import { Path } from "react-hook-form";
-import sharedStyles from "../../../../styles/shared.module.css";
 
 interface Input<T extends FieldValues> {
   id: string;
   name: Path<T>;
   registerField: UseFormRegister<T>;
+  className: string;
 }
 
 export default function Input<T extends FieldValues>({
   id,
   name,
-  registerField
+  registerField,
+  className
 }: Input<T>) {
   return (
     <input
       id={id}
       {...registerField(name)}
       onClick={(e) => e.stopPropagation()}
-      className={sharedStyles.inputElement}
+      className={className}
       data-testid={`testid-${id}`}
     />
   );
